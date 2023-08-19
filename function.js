cf984b7640b1dcaa48666f70205d980b361dca64
Sokhim <lachsokhim77@gmail.com>
2023-08-19 20:26:28 +0700

    using return keyword without else

--- function.js
+++ function.js
@@ -4,11 +4,10 @@ function sum(a,b){
     if(!a || !b) {
         // validation
         total = 0;
-        // return;
-    }else {
-        total = a + b;
-        console.log('Worked')
+        return;
     }
+    total = a + b;
+    console.log('Worked')
 }
 
 sum();
