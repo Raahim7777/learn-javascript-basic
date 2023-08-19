5a237be78357d0ee8c96b9698dce60f22cf7d372
Sokhim <lachsokhim77@gmail.com>
2023-08-19 20:02:35 +0700

    Learn about using function

---
+++ function.js
@@ -0,0 +1,11 @@
+function sum(a,b){
+    if(!a || !b) {
+        // validation
+        return;
+    }
+    return a+b;
+    
+}
+let result = sum(4,6);
+console.log(result)
+console.log(sum(0, 2));
